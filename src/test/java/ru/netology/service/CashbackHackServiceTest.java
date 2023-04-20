e41a7460263a78169dcf6c3e58e557ffaf6beb2d
IIIAMAH4ik <tembatokmakov43@gmail.com>
2023-04-20 11:42:04 +0300

    testng

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,12 +1,17 @@
 package ru.netology.service;
 
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 class CashbackHackServiceTest {
 
     @Test
     void remain() {
+        int amount = 900;
+        int expected = 100;
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(amount);
+        Assert.assertEquals(actual, expected);
     }
+
 }
